5fb4765de232c56f2f5a4a77cf70d389fdb1b2d2
Mahran_AD <mhranabwdqt971@gmail.com>
2025-04-08 01:06:53 +0300

    Make routing inside nav  bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import { SignUp } from "./SignUp";
+import { Login } from "./Login";
 import Header from "./Components/Header";
+
 export default function App() {
-  return(
+  return (
     <div>
-    <Header/>
-    <SignUp/>
+      <Header />
+      <Routes>
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <SignUp />
     </div>
-    )
+  );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,27 @@
-export default function Headers(){
-    return(
-<div>
-    
-</div>
-    );
+import { Link } from "react-router-dom";
+export default function Headers() {
+  return (
+    <div>
+      <nav
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ display: "flex", gap: "20px" }}>
+          <h6>Home</h6>
+          <h6>About</h6>
+        </div>
+        <div style={{ display: "flex", gap: "20px" }}>
+          <Link to="/register" className="register-bar">
+            Register
+          </Link>
+          <Link to="/login" className="register-bar">
+            Login
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }

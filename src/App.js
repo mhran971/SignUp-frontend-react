64bc4ad3e5551ea4ad65cb006be03bae43f4dadc
Mahran_AD <mhranabwdqt971@gmail.com>
2025-04-12 03:24:30 +0300

    header departmentlization for  signup and login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./SignUp";
 import Login from "./Login";
-import Header from "./Components/Header";
 import Home from "./Home";
 import Dashboard from "./Components/Dashboard";
 
 export default function App() {
   return (
     <div>
-      <Header />
+     
       <Routes>
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import "./SignUp.css";
 import React from "react";
+import Header from "./Components/Header";
 
 export default function Login() {
   const [email, setemail] = React.useState("");
@@ -11,8 +12,6 @@ export default function Login() {
   console.log(email);
   console.log(password);
 
-
-  
   async function Submit(e) {
     let flag = true;
     e.preventDefault();
@@ -36,37 +35,40 @@ export default function Login() {
   }
 
   return (
-    <div className="father">
-      <form className="form-father" onSubmit={Submit}>
-        <label htmlFor="email">Email: </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setemail(e.target.value)}
-          placeholder="Email..."
-          requir="true"
-        />
-        {accept && emailError === 422 && (
-          <p className="error">Email has been taken</p>
-        )}
-        <label htmlFor="password">Password: </label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={password}
-          onChange={(e) => setpassword(e.target.value)}
-          placeholder="Password..."
-          requir="true"
-        />
-        {password.length < 8 && accept === true && (
-          <p className="error">Password have to be at least 8 chars</p>
-        )}
+    <div className="login">
+      <Header />
+      <div className="father">
+        <form className="form-father" onSubmit={Submit}>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+            placeholder="Email..."
+            requir="true"
+          />
+          {accept && emailError === 422 && (
+            <p className="error">Email has been taken</p>
+          )}
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setpassword(e.target.value)}
+            placeholder="Password..."
+            requir="true"
+          />
+          {password.length < 8 && accept === true && (
+            <p className="error">Password have to be at least 8 chars</p>
+          )}
 
-        <button type="submit">Submit</button>
-      </form>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }

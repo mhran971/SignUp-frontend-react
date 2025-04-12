d2d49aed07dbb9aa7deb3938d57c857783e4b4e2
Mahran_AD <mhranabwdqt971@gmail.com>
2025-04-12 03:13:18 +0300

    initialize Dashboard & add dash router in app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
-import  SignUp  from "./SignUp";
+import SignUp from "./SignUp";
 import Login from "./Login";
 import Header from "./Components/Header";
 import Home from "./Home";
+import Dashboard from "./Components/Dashboard";
 
 export default function App() {
   return (
@@ -11,10 +12,9 @@ export default function App() {
       <Routes>
         <Route path="/register" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home/>} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/Dashboard" element={<Dashboard />}></Route>
       </Routes>
-      
     </div>
   );
 }

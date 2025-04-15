84d53327141790c231f4012c497cb41dec913598
Mahran_AD <mhranabwdqt971@gmail.com>
2025-04-15 21:53:59 +0300

    adding delete logic

--- src/Users.js
+++ src/Users.js
@@ -1,8 +1,29 @@
 import { useEffect, useState } from "react";
 import "./all.min.css";
+import axios from "axios";
 
 export default function Users() {
   const [users, setUsers] = useState([]);
+  
+  function deleteUser(id){
+    axios.delete(`http://127.0.0.1:8000/api/user/delete/${id}`);
+  }
+  const showUsers = users.map((user, index) => (
+    <tr key={index}>
+      <td>{user.id}</td>
+      <td>{user.name}</td>
+      <td>{user.email}</td>
+      <td style={{ display: "flex", justifyContent: "space-around", paddingRight:"50px"}}>
+        <i
+          style={{ color: "orange" }}
+          class="fa-duotone fa-solid fa-pen-to-square"
+          onClick={()=>deleteUser(user.id)}
+        ></i>
+        <i style={{ color: "red" }} class="fa-solid fa-xmark"></i>
+      </td>
+    </tr>
+  ));
+
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/user/show")
@@ -16,19 +37,6 @@ export default function Users() {
       .then((data) => setUsers(data))
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
-
-  const showUsers = users.map((user, index) => (
-    <tr key={index}>
-      <td>{user.id}</td>
-      <td>{user.name}</td>
-      <td>{user.email}</td>
-      <td style={{ display:"flex" ,justifyContent:"space-around", paddingRight:"50px" ,cursor:"pointer"}}>
-        <i style={{ color:"orange" }} class="fa-duotone fa-solid fa-pen-to-square"></i>
-        <h1> </h1>
-        <i style={{ color:"red" }} class="fa-solid fa-xmark"></i>
-      </td>
-    </tr>
-  ));
 
   return (
     <div>
